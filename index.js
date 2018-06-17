cfe6dc6a9c74b6eec10d68025da8316e249787cc
Jiří Otáhal <xotahal@gmail.com>
2018-06-17 18:16:56 +1200

    Add scale animation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ export { default as SharedElement } from './src/SharedElement';
 export { default as SharedElementRenderer } from './src/SharedElementRenderer';
 // Available Animations
 export { default as ScaleAndOpacity } from './src/ScaleAndOpacity';
+export { default as Scale } from './src/Scale';
 export { default as TranslateYAndOpacity } from './src/TranslateYAndOpacity';
 export { default as TranslateY } from './src/TranslateY';
 export { default as TranslateX } from './src/TranslateX';
---
+++ src/Scale.js
@@ -0,0 +1,66 @@
+import React, { PureComponent } from 'react';
+import { Animated, InteractionManager } from 'react-native';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  type: PropTypes.string,
+  animateOnDidMount: PropTypes.bool,
+};
+const defaultProps = {
+  type: 'timing',
+  animateOnDidMount: false,
+};
+
+class Scale extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const { value, initValue } = props;
+
+    this.state = {
+      scaleValue: new Animated.Value(initValue || value),
+    };
+  }
+  componentDidMount() {
+    if (this.props.animateOnDidMount) {
+      InteractionManager.runAfterInteractions().then(() => {
+        this.move(this.props);
+      });
+    }
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.value !== nextProps.value) {
+      this.move(nextProps);
+    }
+  }
+  move = props => {
+    const { value, type, onShowComplete, ...rest } = props;
+    const { scaleValue } = this.state;
+
+    Animated[type](scaleValue, {
+      toValue: value,
+      ...rest,
+    }).start(() => {
+      if (onShowComplete) {
+        onShowComplete(props);
+      }
+    });
+  };
+  render() {
+    const { style, children } = this.props;
+    const { scaleValue } = this.state;
+
+    const animatedStyle = {
+      transform: [{ scale: scaleValue }],
+    };
+
+    return (
+      <Animated.View style={[style, animatedStyle]}>{children}</Animated.View>
+    );
+  }
+}
+
+Scale.propTypes = propTypes;
+Scale.defaultProps = defaultProps;
+
+export default Scale;
