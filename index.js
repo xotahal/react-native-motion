658a8b3849684eedc674ee309185a661017f5700
Jiří Otáhal <xotahal@gmail.com>
2018-06-22 10:51:21 +1200

    Add animated number

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 export { default as SharedElement } from './src/SharedElement';
 export { default as SharedElementRenderer } from './src/SharedElementRenderer';
 // Available Animations
+export { default as AnimatedNumber } from './src/AnimatedNumber';
 export { default as ScaleAndOpacity } from './src/ScaleAndOpacity';
 export { default as Scale } from './src/Scale';
 export { default as TranslateYAndOpacity } from './src/TranslateYAndOpacity';
---
+++ src/AnimatedNumber.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Animated } from 'react-native';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  type: PropTypes.string,
+};
+const defaultProps = {
+  type: 'timing',
+};
+/**
+ */
+class AnimatedNumber extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    const animatedValue = new Animated.Value(props.value);
+    animatedValue.addListener(this.onValueChanged);
+
+    this.state = {
+      animatedValue,
+      value: props.value,
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.value !== nextProps.value) {
+      this.move(nextProps);
+    }
+  }
+  onValueChanged = e => {
+    this.setState({
+      value: e.value,
+    });
+  };
+  move = props => {
+    const { value, style, type, ...rest } = props;
+
+    Animated[type](this.state.animatedValue, {
+      toValue: value,
+      ...rest,
+    }).start();
+  };
+  render() {
+    const { renderValue } = this.props;
+    const { value } = this.state;
+
+    return renderValue(value);
+  }
+}
+
+AnimatedNumber.propTypes = propTypes;
+AnimatedNumber.defaultProps = defaultProps;
+
+export default AnimatedNumber;
